dcfa1f22a311e04028b16968fb9d5fc77fa14512
toby <tobygeneva@hotmail.com>
2026-01-29 13:54:19 +0100

    Initial Commit

---
+++ emi3/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  emi3
+//
+//  Created by Toby Simpson on 29.01.2026.
+//
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return EXIT_SUCCESS;
+}
