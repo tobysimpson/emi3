04c3dbe6614b58c527cbd1a8c731a4f2f0d42425
toby <tobygeneva@hotmail.com>
2026-01-29 13:55:27 +0100

    begun

--- emi3/main.c
+++ emi3/main.c
@@ -8,8 +8,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+//working with mesh
+int main(int argc, const char * argv[])
+{
+    printf("hello\n");
+    
+    printf("done\n");
     return EXIT_SUCCESS;
 }
