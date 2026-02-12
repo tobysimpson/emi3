6a2ac24e64f92f3fa22cb498eb07076226f90144
toby <tobygeneva@hotmail.com>
2026-02-12 17:07:01 +0100

    writes tet and voxel

---
+++ emi3/host/tet.h
@@ -0,0 +1,12 @@
+//
+//  tet.h
+//  emi3
+//
+//  Created by Toby Simpson on 12.02.2026.
+//
+
+#ifndef tet_h
+#define tet_h
+
+
+#endif /* tet_h */
---
+++ emi3/host/vxl.h
@@ -0,0 +1,12 @@
+//
+//  vxl.h
+//  emi3
+//
+//  Created by Toby Simpson on 12.02.2026.
+//
+
+#ifndef vxl_h
+#define vxl_h
+
+
+#endif /* vxl_h */
